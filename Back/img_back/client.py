c93729711565f794cc70c7124adbebc3e88a6441
Haley <72124829+Haley0101@users.noreply.github.com>
2023-04-03 01:41:22 +0900

    v 0.2

--- Back/img_back/client.py
+++ Back/img_back/client.py
@@ -1,7 +1,17 @@
 import requests
 
-files = open('goooood.png', 'rb')
+file1 = open('1.png', 'rb')
+file2 = open('2.png', 'rb')
+file3 = open('3.png', 'rb')
+file4 = open('4.png', 'rb')
+file5 = open('5.png', 'rb')
 
-upload = {'file': files}
+upload = {
+    'file1': file1,
+    'file2': file2,
+    'file3': file3,
+    'file4': file4,
+    'file5': file5
+    }
 
-res = requests.post('http://127.0.0.1:8080/ai/check-face', files = upload)
+res = requests.post('http://127.0.0.1:9090/ai/check-face', files = upload)
--- Back/img_back/server.py
+++ Back/img_back/server.py
@@ -2,12 +2,18 @@ from flask import *
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+files = []
 
 # HTTP POST방식으로 전송된 이미지를 저장
-@app.route('/', methods=['POST'])
-def save_image():
-    f = request.files['file']
-    f.save('./save_image/' + secure_filename(f.filename))
+@app.route('/ai/check-face', methods=['POST'])
+def save_images():
+    files.append(request.files['file1'])
+    files.append(request.files['file2'])
+    files.append(request.files['file3'])
+    files.append(request.files['file4'])
+    files.append(request.files['file5'])
+    for f in files:
+        f.save('./save_image/' + secure_filename(f.filename))
     return 'done!'
 
 if __name__ == "__main__":
